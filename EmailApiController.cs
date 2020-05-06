1bc78244c2ebe54aa9eba729018db6d6dc05ec24
JSol <32881231+Jiraso@users.noreply.github.com>
2020-05-06 15:21:26 -0700

    Add files via upload

---
+++ EmailApiController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sabio.Models.Domain;
+using Sabio.Models.Requests.Email;
+using Sabio.Services;
+using Sabio.Services.Interfaces;
+using Sabio.Web.Controllers;
+using Sabio.Web.Models.Responses;
+
+namespace Sabio.Web.Api.Controllers
+{
+    [Route("api/email")]
+    public class EmailApiController : BaseApiController
+    {
+        private IAuthenticationService<int> _authService = null;
+        private IEmailService _emailService = null;
+        private IOrganizationService _organizationService = null;
+        private IPaymentAccountsService _paymentAccountService = null;
+
+        public EmailApiController(
+            IAuthenticationService<int> authService,
+            IEmailService emailService,
+            IOrganizationService organizationService,
+            IPaymentAccountsService paymentAccountService,
+            ILogger<EmailApiController> logger) : base(logger)
+        {
+            _authService = authService;
+            _emailService = emailService;
+            _organizationService = organizationService;
+            _paymentAccountService = paymentAccountService;
+            
+        }
+
+        [HttpPost("send")]
+        public async Task<ActionResult<SuccessResponse>> ExecuteAsync(EmailAddRequest model)
+        {
+            BaseResponse result;
+            int statusCode = 200;
+
+            try
+            {
+                await _emailService.Execute(model);
+                result = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                base.Logger.LogError(ex.ToString());
+                result = new ErrorResponse(ex.Message);
+                statusCode = 500;
+            }
+            return StatusCode(statusCode, result);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<SuccessResponse>> RegisterConfirm(string email, string token) 
+        {
+            BaseResponse result;
+            int sCode = 200;
+
+            try
+            {
+                await _emailService.RegisterConfirm(email, token);
+                result = new SuccessResponse();
+            }
+            catch (Exception ex) 
+            {
+                base.Logger.LogError(ex.ToString());
+                result = new ErrorResponse(ex.Message);
+                sCode = 500;
+            }
+            return StatusCode(sCode, result);
+        }
+
+        [HttpPost("lostinfo")]
+        public async Task<ActionResult<SuccessResponse>> LostInfoEmail(EmailLostInfoSenderRequest model) 
+        {
+            BaseResponse response = null;
+            int sCode = 200;
+            int userId = _authService.GetCurrentUserId();
+            string orgEmail = null;
+
+            try
+            {
+                List<UserOrganization> userOrgs = _organizationService.GetForStolenInfo(userId, model.AccountNumber);
+
+                if (userOrgs == null && !model.IsActive)
+                {
+                    sCode = 404;
+                    response = new ErrorResponse("Payment account already deactivated");
+                }
+                else if (userOrgs == null && model.IsActive) 
+                {
+                    _paymentAccountService.UpdateActiveStatus(userId, model.AccountNumber, model.PaymentTypeId);
+                    response = new SuccessResponse();
+                }
+                else
+                {
+                    foreach (var userOrg in userOrgs)
+                    {
+                        int indexOfSiteStart = userOrg.SiteUrl.IndexOf("w.");
+                        string orgSite = userOrg.SiteUrl.Substring(indexOfSiteStart + 2);
+                        //orgEmail = $"support@{orgSite}";
+                        orgEmail = "v2q2h8z7w6j0y6o1@sabionation.slack.com";
+
+                        if (userOrg.PaymentAccountIsActive && model.IsActive && model.AccountNumber == userOrg.AccountNumber && model.PaymentTypeId == userOrg.PaymentTypeId)
+                        {
+                            _paymentAccountService.UpdateActiveStatus(userId, model.AccountNumber, model.PaymentTypeId);
+                            await _emailService.LostInfoEmail(model, orgEmail);
+                            response = new SuccessResponse();
+                        }
+                        else
+                        {
+                            sCode = 404;
+                            response = new ErrorResponse("Payment account already deactivated or Not linked to organization");
+                        };
+                    }
+                }
+            }
+            catch(Exception ex) 
+            {
+                base.Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message);
+                sCode = 500;
+            };
+            return StatusCode(sCode, response);
+        }
+
+
+    }
+
+}
---
+++ EmailService.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Sabio.Models.AppSettings;
+using Sabio.Models.Requests.Email;
+using Sabio.Services.Interfaces;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Configuration;
+using System.IO;
+using System.Net.Mail;
+using Sabio.Models.Requests;
+using System.Net;
+
+namespace Sabio.Services
+{
+    public class EmailService : IEmailService
+    {
+        private SendGridConfig _config;
+
+        public EmailService(IOptions<SendGridConfig> config) 
+        {
+           _config = config.Value; 
+        }
+        public async Task Execute(EmailAddRequest model)
+        {
+         
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(model.FromEmail, model.FromName),
+                Subject = model.Subject,
+                PlainTextContent = model.Content,
+                HtmlContent = $"<strong>{model.Content}</strong>"
+            };
+            msg.AddTo(new EmailAddress("v2q2h8z7w6j0y6o1@sabionation.slack.com", "Test User"));
+            await SendEmail(msg);
+        }
+
+        public async Task RegisterConfirm(string email, string token)
+        {
+            string templateLocation = "EmailTemplates\\RegisterConfirm.html";
+            string currDir = Directory.GetCurrentDirectory();
+            string path = Path.Combine(currDir, templateLocation);
+            string htmlContent = System.IO.File.ReadAllText(path);
+            htmlContent = htmlContent.Replace("{@Token}", token);
+       
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(_config.Email, _config.Sender),
+                Subject = "Welcome!",
+                HtmlContent = htmlContent
+            };
+
+            msg.AddTo(new EmailAddress(email));
+            await SendEmail(msg);
+        }
+        public async Task PasswordChange(string email, string token)
+        {
+            string templateLocation = "EmailTemplates\\UpdatePassword.html";
+            string currDir = Directory.GetCurrentDirectory();
+            string path = Path.Combine(currDir, templateLocation);
+            string htmlContent = System.IO.File.ReadAllText(path);
+            htmlContent = htmlContent.Replace("{@Token}", token);
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(_config.Email, _config.Sender),
+                Subject = "Update Password",
+                HtmlContent = htmlContent
+            };
+
+            msg.AddTo(new EmailAddress(email));
+            await SendEmail(msg);
+        }
+    
+        public async Task LostInfoEmail(EmailLostInfoSenderRequest model, string toEmail) 
+        {
+            string accountNumber = model.AccountNumber;
+            string accountLastFour = null;
+            if (model.AccountNumber.Length == 16)
+            {
+                accountLastFour = model.AccountNumber.Substring(12);
+            }
+            else if (model.AccountNumber.Length == 15)
+            {
+                accountLastFour = model.AccountNumber.Substring(11);
+            }
+            else if (model.AccountNumber.Length == 14) 
+            {
+                accountLastFour = model.AccountNumber.Substring(10);
+            }
+            else if (model.AccountNumber.Length == 12)
+            {
+                accountLastFour = model.AccountNumber.Substring(8);
+            }
+
+            string templateLocation = "EmailTemplates\\LostInformation.html";
+            string currDir = Directory.GetCurrentDirectory();
+            string path = Path.Combine(currDir, templateLocation);
+            string htmlContent = System.IO.File.ReadAllText(path);
+            htmlContent = htmlContent.Replace("{@SenderFirstName}", model.SenderFirstName);
+            htmlContent = htmlContent.Replace("{@SenderLastName}", model.SenderLastName);
+            htmlContent = htmlContent.Replace("{@AccountLastFour}", accountLastFour);
+            htmlContent = htmlContent.Replace("{@CardType}", model.CardType);
+            htmlContent = htmlContent.Replace("{@PhoneNumber}", model.PhoneNumber);
+            htmlContent = htmlContent.Replace("{@Email}", model.FromEmail);
+
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(model.FromEmail, model.SenderFirstName),
+                Subject = "Request for new information",
+                HtmlContent = htmlContent
+            };
+            msg.AddTo(new EmailAddress(toEmail));
+            await SendEmail(msg);
+        }
+
+       
+        private async Task SendEmail(SendGridMessage msg) 
+        {
+            var apiKey = _config.SendGridApiKey;
+            var client = new SendGridClient(apiKey);
+            await client.SendEmailAsync(msg);          
+        }
+
+        
+
+    }
+}
